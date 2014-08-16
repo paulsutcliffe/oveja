8e80e09123f5cf68f649f4aa077fb458204b97d5
Paul Sutcliffe <paul@kosmyka.com>
2014-08-16 00:25:54 -0500

    Layout and home slider

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,25 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require turbolinks
+// require_tree .
+//
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require jquery.migrate.js
+//= require modernizrr.js
 //= require bootstrap-sprockets
-//= require_tree .
+//= require jquery.fitvids.js
+//= require owl.carousel.min.js
+//= require nivo-lightbox.min.js
+//= require jquery.isotope.min.js
+//= require jquery.appear.js
+//= require count-to.js
+//= require jquery.textillate.js
+//= require jquery.lettering.js
+//= require jquery.easypiechart.min.js
+//= require jquery.nicescroll.min.js
+//= require jquery.parallax.js
+//= require jquery.themepunch.plugins.min.js
+//= require jquery.themepunch.revolution.min.js
+//= require script.js
